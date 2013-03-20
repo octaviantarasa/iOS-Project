88a57bf1241c6faa5beab79648980e1ceaddfcfc
cdalxndr <cdalxndr@yahoo.com>
2013-03-20 19:55:49 +0200

    Prima pagina

---
+++ FirstPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  FirstPage.m
+//  Landscapes Location
+//
+//  Created by Laborator iOS on 3/20/13.
+//  Copyright (c) 2013 Laborator iOS. All rights reserved.
+//
+
+#import "FirstPageViewController.h"
+
+@interface FirstPageViewController ()
+
+@end
+
+@implementation FirstPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
