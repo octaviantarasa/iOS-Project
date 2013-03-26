868b4d1e8a8bae060450f28e7558a70d70d3fba2
anonim <no@email.com>
2013-03-26 17:19:53 +0200

    Create Account+SignIn

---
+++ FirstPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstPage.h
+//  Landscapes Location
+//
+//  Created by Laborator iOS on 3/20/13.
+//  Copyright (c) 2013 Laborator iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstPageViewController : UIViewController
+- (IBAction)createAccount:(id)sender;
+- (IBAction)signIn:(id)sender;
+
+@end
---
+++ Landscapes Location/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Landscapes Location
+//
+//  Created by Laborator iOS on 3/20/13.
+//  Copyright (c) 2013 Laborator iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
